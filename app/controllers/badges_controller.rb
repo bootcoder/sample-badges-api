41be4d411ed2a789639a008ea2a41f80d6389494
bootcoder <bootcoder@gmail.com>
2016-06-22 02:24:56 -0700

    badges#create

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,2 +1,18 @@
 class BadgesController < ApplicationController
+
+  def create
+    @badge = Badge.new(badge_params)
+    if @badge.save
+      render json: @badge
+    else
+      render json @badge.errors.full_messages
+    end
+  end
+
+  private
+
+  def badge_params
+    params.require(:badge).permit(:phrase, :vote_type, :teacher_id)
+  end
+
 end
