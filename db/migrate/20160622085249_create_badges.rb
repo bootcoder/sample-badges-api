fe7e8cc5c347a8e1e6a89b42c9fd80a10e6551b1
bootcoder <bootcoder@gmail.com>
2016-06-22 02:06:11 -0700

    teachers#index

--- db/migrate/20160622085249_create_badges.rb
+++ db/migrate/20160622085249_create_badges.rb
@@ -1,7 +1,9 @@
 class CreateBadges < ActiveRecord::Migration
   def change
     create_table :badges do |t|
-
+      t.string :phrase
+      t.references :teacher
+      t.integer :votes
       t.timestamps null: false
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Teacher.create(name: "Hunter")
+Teacher.create(name: "Derek")
+Teacher.create(name: "Seba")
+Teacher.create(name: "Shambhavi")
+Teacher.create(name: "Ken")
+Teacher.create(name: "Walker")
+Teacher.create(name: "Julian")
+Teacher.create(name: "Jaclyn")
+Teacher.create(name: "Jen")
+Teacher.create(name: "Sherif")
